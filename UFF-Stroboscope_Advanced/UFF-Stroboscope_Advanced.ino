fac021d5eacd7a08529c2cda76d129cd9a86093a
Andrey <andrey.udeneew@mail.ru>
2022-07-04 04:15:47 +0300

    v0.9 Interrupts PWM switching, UART PWM tuning were added. 4 approwing.

--- UFF-Stroboscope_Advanced/UFF-Stroboscope_Advanced.ino
+++ UFF-Stroboscope_Advanced/UFF-Stroboscope_Advanced.ino
@@ -3,38 +3,11 @@
 
 String cmd, CMDcur;
 int programNumber;
-int WL_NUM = 2;   //Number of Wavelengths (LED drivers connected), should not be higher than 7
-int PWM_White = 20;
+int counter = 0;
+int PWM_White = 2;
 int PWM_UV = 250;
 int UV_LED = 5;
 int WHITE_LED = 6;
-int PWMvals[pinsNum] = {0, 5, 250}; // 0 - 255
-int steps[pinsNum] = {0, 1, 2}; // 0 = Dunkelheit, 1 - white, 2 - UV, 3 - RED
-bool pinsPWM[pinsNum] = {true, true, true}; // pins with PWM are 3,5,6,9,10,11
-int pins[pinsNum] = {3, 5, 6};
-int pinsCombinations0[stepsNum][pinsNum] =
-{
-  {1, 0},
-  {1, 1}
-};
-int pinsCombinations1[stepsNum][pinsNum] =
-{
-  {0, 0, 0},
-  {0, 1, 1},
-  {0, 0, 1}
-};
-int pinsCombinations2[stepsNum][pinsNum] =
-{
-  {1, 0, 0},
-  {0, 1, 0},
-  {0, 0, 1}
-};
-int pinsCombinations3[stepsNum][pinsNum] =
-{
-  {0, 0, 0},
-  {0, 1, 0},
-  {0, 0, 1}
-};
 
 void setup()
 {
@@ -48,67 +21,61 @@ void setup()
   analogWrite(WHITE_LED, PWM_White);
   Serial.begin(115200);
   Serial.setTimeout(100);
-  while (!Serial) {;}
+  attachInterrupt(0, Strob_Input_Handler, RISING);
+  while (!Serial) {
+    ;
+  }
 }
 
+
+void Strob_Input_Handler() {
+  if(counter == 1){
+analogWrite(UV_LED, PWM_UV);    
+  }
+  else{
+  analogWrite(UV_LED, 0);  
+  }
+    counter++;  // + синхр.
+  if(counter == 2){
+    counter = 0;
+  }
+}
 void waiting_4_command() {
-  char num_of_cyckles, numOFpinsH, numOFpinsL;
-  String num_of_cyckles_str;
+  int PWM_VAL, PWM_VALH, PWM_VALL;
   cmd = "";
   if (Serial.available()) {
     cmd = Serial.readString();
     cmd.trim();
   }
 
-    if (cmd.substring(0, 4) == "MODE") {
-        programNumber = cmd[4] - '0';               
-    cmd = "changeProgram";
-    CMDcur = "changeProgram";
-  }
-}
 
-int CurState = 0;
-int PrevState = 0;
-int curStep = 0, lampc = 0;
-int LampState = 0;
+    if (cmd.substring(0, 2) == "UV") {
+        PWM_VALH = cmd[2] - '0';
+        PWM_VALL = cmd[3] - '0';
+        if(cmd[3] != '\0'){
+        PWM_VAL = (PWM_VALH*10) + (PWM_VALL*1);
+        }
+        else{
+         PWM_VAL = (PWM_VALH*1) + (PWM_VALL*0); 
+        }        
+PWM_UV = PWM_VAL;
+  }
+
+    if (cmd.substring(0, 2) == "WH") {
+        PWM_VALH = cmd[2] - '0';
+        PWM_VALL = cmd[3] - '0';
+        if(cmd[3] != '\0'){
+        PWM_VAL = (PWM_VALH*10) + (PWM_VALL*1);
+        }
+        else{
+         PWM_VAL = (PWM_VALH*1) + (PWM_VALL*0); 
+        }        
+PWM_White = PWM_VAL;
+analogWrite(WHITE_LED, PWM_White);
+  }
+  }
+
 void loop()
 {
-  CurState = digitalRead(2);
-
-  //Here we manage LEDS to work sequentially
-  if (CurState < PrevState)
-  {
-    //    digitalWrite(5 + curStep, LOW);
-    //        analogWrite(5 + curStep, 0);
-    //          analogWrite(UV_LED, 0);
-
-    lampc++;
-    curStep++;
-    if (curStep >= WL_NUM)
-      curStep = 0;
-    //    digitalWrite(5 + curStep, HIGH);
-    //        analogWrite(5 + curStep, PWM_UV);
-    //          analogWrite(UV_LED, PWM_UV);
-
-    for (uint8_t LED = 0; LED < pinsNum; LED += 1)
-    {
-      analogWrite(pins[LED], (PWMvals[LED])*pinsCombinations1[curStep][LED]);
-    }
-  }
-
-  //Here Loop control signal
-  if (CurState > PrevState && curStep == 0)
-    digitalWrite(5 + WL_NUM, HIGH);
-  if (CurState < PrevState && curStep == 0)
-    digitalWrite(5 + WL_NUM, LOW);
-
-  //Arduino onboard lamp flashing if frames are arriving; for debug purposes
-  if (lampc > 20)
-  {
-    LampState = !LampState;
-    digitalWrite(13, LampState);
-    lampc = 0;
-  }
-
-  PrevState = CurState;
+  waiting_4_command();
 }
